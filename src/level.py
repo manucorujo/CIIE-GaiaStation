3b01a6ee5c0de0f4b4a71b519ff10253d4fc483a
agr17 <aregueiro1700@gmail.com>
2022-02-22 23:53:32 +0100

    Movimiento de cámara

--- src/level.py
+++ src/level.py
@@ -16,7 +16,7 @@ class Level:
         self.display_surface = pygame.display.get_surface()
 
         # Grupos de sprites
-        self.visible_sprites = pygame.sprite.Group()
+        self.visible_sprites = YSortCameraGroup()
         self.obstacle_sprites = pygame.sprite.Group()
 
         self.create_map()
@@ -37,6 +37,34 @@ class Level:
 
     def run(self):
         # mostrar os sprites dentro do grupo "visible_sprites"
-        self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
-        debug(self.player.direction)
+        # debug(self.player.direction)
+
+'''
+YSort es porque vamos a ordenar los sprites por la coordenada y, de esa forma le daremos algo de overlap
+'''
+class YSortCameraGroup(pygame.sprite.Group):
+    def __init__(self):
+
+        # general setup
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+        self.half_width = self.display_surface.get_size()[0] // 2
+        self.half_height = self.display_surface.get_size()[1] // 2
+        self.offset = pygame.math.Vector2() # [x:0, y:0]
+    
+    def custom_draw(self,player):
+
+        '''
+        Un poco de geometria y se entiende, jurado
+        '''
+
+        # getting the offset
+        self.offset.x = player.rect.centerx - self.half_width
+        self.offset.y = player.rect.centery - self.half_height
+
+        for sprite in self.sprites():
+            offset_pos = sprite.rect.topleft - self.offset
+            self.display_surface.blit(sprite.image, offset_pos)
+        
